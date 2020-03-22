83e84a84a7d2486bb99369200221af5ea0e10f9c
s19584 <61684577+s19584@users.noreply.github.com>
2020-03-22 17:58:48 +0100

    praca domowa v.001
    platforma ruszająca się

---
+++ Praca_domowa_1/Assets/player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public float speed = 500;
+    public float jump = 400;
+    private bool isGrounded;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+        isGrounded = true;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        float xDisplacement = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        rb.velocity = new Vector2(xDisplacement, rb.velocity.y);
+
+        if(Input.GetKeyDown("space") && (isGrounded == true)) {
+          rb.AddForce(new Vector2(0, jump));
+          isGrounded = false;
+        }
+    }
+    
+    void OnCollisionEnter2D(Collision2D col){
+     isGrounded = true;
+     }
+}
