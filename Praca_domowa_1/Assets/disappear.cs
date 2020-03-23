a3bbc421e1652b81a1e7494108b9bc70bb0b4a83
s19584 <61684577+s19584@users.noreply.github.com>
2020-03-23 07:56:47 +0100

    Praca domowa v.002
    działa platforma znikająca
    spin nie działa i nie umiem programować ._.

---
+++ Praca_domowa_1/Assets/disappear.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class disappear : MonoBehaviour
+{
+    public float Time = 2;
+    Rigidbody2D rb;
+    
+
+void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+      {
+         if(collision.gameObject.name.Equals("player"))
+            {
+                Destroy(gameObject,Time);
+            }
+      }
+
+}
---
+++ Praca_domowa_1/Assets/spin.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spin : MonoBehaviour
+{
+    [SerializeField]
+    Transform rotationCenter;
+    [SerializeField]
+    float rotationRadius = 2f, angularSpeed = 2f;
+    float posX, posY, angle = 0f;
+   
+   
+   void Update()
+    {
+        posX = rotationCenter.position.x + Mathf.Cos(angle) * rotationRadius;
+        posY = rotationCenter.position.y + Mathf.Sin(angle) * rotationRadius;
+        angle = angle + Time.deltaTime * angularSpeed;
+        if (angle >= 360f)
+            angle = 0f;
+    }
+}
