52e7febd473a0090515ce0d0b4f4132978daa0b9
s19584 <61684577+s19584@users.noreply.github.com>
2020-03-23 10:43:40 +0100

    praca domowa v.003
    DZIAŁA SPIN

--- Praca_domowa_1/Assets/spin.cs
+++ Praca_domowa_1/Assets/spin.cs
@@ -3,20 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class spin : MonoBehaviour
+
 {
-    [SerializeField]
-    Transform rotationCenter;
-    [SerializeField]
-    float rotationRadius = 2f, angularSpeed = 2f;
-    float posX, posY, angle = 0f;
-   
-   
-   void Update()
+    private bool collide;
+    void Update () {
+      if(collide == true)
+        {
+         transform.Rotate(Vector3.forward * 1);
+        }
+ }
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        posX = rotationCenter.position.x + Mathf.Cos(angle) * rotationRadius;
-        posY = rotationCenter.position.y + Mathf.Sin(angle) * rotationRadius;
-        angle = angle + Time.deltaTime * angularSpeed;
-        if (angle >= 360f)
-            angle = 0f;
+        collide = true;
     }
 }
