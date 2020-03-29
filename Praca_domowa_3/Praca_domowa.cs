2ac49908de2eb3e2705b41aa4be3f5d46712ae01
s19584 <61684577+s19584@users.noreply.github.com>
2020-03-30 01:17:41 +0200

    Praca_domowa_3 v.001
    Przynamniej próbowałem.

---
+++ Praca_domowa_3/Praca_domowa.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Praca_domowa : MonoBehaviour{
+  
+     void Start() {
+
+  int intA = 5;
+  float floatie = 3;
+  bool boolA = true;
+  bool boolB = false;
+  string strA = "Programuję w C#";
+  string strB = "EPG";
+
+   Debug.Log("intA: " + intA + " floatie: " + floatie + " boolA: " + boolA + " boolB: " + boolB + " strA: " + strA + " strB: " + strB);
+      
+      if (intA == floatie){
+       Debug.Log = "Tak";
+           } else {
+       Debug.Log = "Nie";
+       }
+
+       if (intA != floatie){
+       Debug.Log = "Tak"
+           } else {
+       Debug.Log = "Nie";
+      }
+
+
+       if (intA > floatie){
+       Debug.Log = "Tak";
+          } else {
+       Debug.Log = "Nie";
+      }
+
+       if (intA >= floatie){
+       Debug.Log = "Tak";
+          } else {
+       Debug.Log = "Nie";
+      }
+        
+         if (intA < floatie){
+       Debug.Log = "Tak";
+          } else {
+       Debug.Log = "Nie";
+      }
+
+       if (intA <= floatie){
+       Debug.Log = "Tak";
+          } else {
+       Debug.Log = "Nie";
+      }
+
+      if (strA == strB){
+      } else {
+       Debug.Log = "różne";
+	  }
+
+
+
+    }
+        
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
